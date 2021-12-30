10e5a6c9543e75074791885e1db88c8a24d849c7
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-12-30 12:16:16 -0300

    Update app definition

--- src/index.tsx
+++ src/index.tsx
@@ -25,32 +25,42 @@ app({
     baseState,
     JsonFetcher('https://jsonplaceholder.typicode.com/users', GotNames),
   ],
-  view: (state) => html`
-    <main>
-      ${state.names &&
-      html`<ul class="person-list" data-cy="person-list">
-        ${state.names.map((name, index) =>
-          person({
-            name,
-            highlight: state.highlight[index],
-            selected: state.selected === index,
-            ontoggle: [ToggleHighlight, index],
-            onselect: [Select, index],
-          })
-        )}
-      </ul>`}
-      ${state.bio &&
-      state.bio.length > 0 &&
-      html`<div data-cy="bio">${state.bio}</div>`}
-    </main>
-  `,
-  subscriptions: (state) => [
-    state.selected !== null &&
-      state.selected > 0 &&
-      onKeydown('ArrowUp', SelectUp),
-    state.selected !== null &&
-      state.selected < state.ids.length - 1 &&
-      onKeydown('ArrowDown', SelectDown),
-  ],
+  view: (state) => {
+    const gotBio =
+      state.bio !== null &&
+      typeof state.bio === 'string' &&
+      state.bio.length > 0;
+
+    const gotNames = state.names !== null && Array.isArray(state.names);
+
+    return html`
+      <main>
+        ${gotNames &&
+        html`<ul class="person-list" data-cy="person-list">
+          ${state.names!.map((name, index) =>
+            person({
+              name,
+              highlight: state.highlight[index],
+              selected: state.selected === index,
+              ontoggle: [ToggleHighlight, index],
+              onselect: [Select, index],
+            })
+          )}
+        </ul>`}
+        ${gotBio && html`<div data-cy="bio">${state.bio}</div>`}
+      </main>
+    `;
+  },
+  subscriptions: (state) => {
+    const topmostPersonNotSelected =
+      state.selected !== null && state.selected > 0;
+    const bottomPersonNotSelected =
+      state.selected !== null && state.selected < state.ids.length - 1;
+
+    return [
+      topmostPersonNotSelected && onKeydown('ArrowUp', SelectUp),
+      bottomPersonNotSelected && onKeydown('ArrowDown', SelectDown),
+    ];
+  },
   node: document.getElementById('root')!,
 });
